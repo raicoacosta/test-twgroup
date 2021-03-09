887e8bfa19eb2a38b04073d81fc41d0eb02f0802
Raico Acosta <raico26@gmail.com>
2021-03-09 15:05:42 -0500

    Feature: envio de mail

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -14,12 +14,14 @@ class CommentController extends Controller
 
 
     public function store (Request $request, $id) {
+    	$publicacion = Publication::find($id);
 		$comment = Comment::create([
 	        'content' => $request->content,
-	        'publication_id' => Publication::find($id)->id,
+	        'publication_id' => $publicacion->id,
 	        'user_id' => auth()->user()->id,
 	        'status' => $this->defatultStatus,
 	    ]);
+	    Mail::to($publicacion->user->email)->send(new DemoEmail());
 	    return redirect()->route('publication.show', $id);
     }
 }
---
+++ resources/views/mails/newcomment.blade.php
@@ -0,0 +1,3 @@
+Hola 
+<p>Acaban de hacer un comentario en tu publicacion</p>
+ 
